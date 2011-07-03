cab7eea04761402fcab9b89b499b26c9f61a6521
Pat McDonough <pat.mcdonough@redhat.com>
2011-07-02 23:59:20 -0700

    some hackery

--- linux/src/main/resources/ks/no-https-in-ks-hack.sh
+++ linux/src/main/resources/ks/no-https-in-ks-hack.sh
@@ -1,4 +1,6 @@
 #/bin/sh
+# This script lives at https://raw.github.com/CheffPJ/jboss-as/linux_init/linux/src/main/resources/ks/base-ks.cfg, but ks doesn't like https
+
 
 cd /tmp
 wget https://raw.github.com/CheffPJ/jboss-as/linux_init/linux/src/main/resources/ks/f15-white-rabbit-ks.cfg
