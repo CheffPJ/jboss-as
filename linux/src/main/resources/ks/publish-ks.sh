96d133af601e67f8dd83b2b26bfa51fa46539548
Pat McDonough <pat.mcdonough@redhat.com>
2011-07-06 15:53:05 -0700

    new push script

--- linux/src/main/resources/ks/publish-ks.sh
+++ linux/src/main/resources/ks/publish-ks.sh
@@ -1,8 +1,13 @@
 #/bin/sh
 # This script lives at https://raw.github.com/CheffPJ/jboss-as/linux_init/linux/src/main/resources/ks/base-ks.cfg, but ks doesn't like https
 
+export TMP_DIR=/tmp/push-as7-linux
 
-cd /tmp
-rm lovelock-white-rabbit-ks.cfg 2>/dev/null
-wget https://raw.github.com/CheffPJ/jboss-as/linux_init/linux/src/main/resources/ks/lovelock-white-rabbit-ks.cfg
-scp lovelock-white-rabbit-ks.cfg people.redhat.com:/home/remote/pmcdonou/public_html/src/ks/
+rm -rf $TMP_DIR 2>/dev/null
+mkdir -p $TMP_DIR
+cd $TMP_DIR
+
+wget https://raw.github.com/CheffPJ/jboss-as/linux_init/linux/src/main/resources/ks/lovelock-white-rabbit-ks.cfg https://raw.github.com/CheffPJ/jboss-as/linux_init/linux/src/main/resources/etc/rsyslog.d/jboss-as.conf https://raw.github.com/CheffPJ/jboss-as/linux_init/linux/src/main/resources/lib/systemd/system/jboss-as.service
+
+
+scp * people.redhat.com:/home/remote/pmcdonou/public_html/src/jboss-as7-linux/
