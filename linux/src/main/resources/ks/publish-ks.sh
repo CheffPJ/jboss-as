a5606903ed2459330a7794da96627bb788e46b13
Pat McDonough <pat.mcdonough@redhat.com>
2011-07-08 01:10:09 -0700

    fun with words

--- linux/src/main/resources/ks/publish-ks.sh
+++ linux/src/main/resources/ks/publish-ks.sh
@@ -7,7 +7,7 @@ rm -rf $TMP_DIR 2>/dev/null
 mkdir -p $TMP_DIR
 cd $TMP_DIR
 
-wget https://raw.github.com/CheffPJ/jboss-as/linux_init/linux/src/main/resources/ks/lovelock-white-rabbit-ks.cfg https://raw.github.com/CheffPJ/jboss-as/linux_init/linux/src/main/resources/etc/rsyslog.d/jboss-as.conf https://raw.github.com/CheffPJ/jboss-as/linux_init/linux/src/main/resources/lib/systemd/system/jboss-as.service
+wget https://raw.github.com/CheffPJ/jboss-as/linux_init/linux/src/main/resources/ks/white-rabbit-lovelock-ks.cfg https://raw.github.com/CheffPJ/jboss-as/linux_init/linux/src/main/resources/etc/rsyslog.d/jboss-as.conf https://raw.github.com/CheffPJ/jboss-as/linux_init/linux/src/main/resources/lib/systemd/system/jboss-as.service
 
 
 scp * people.redhat.com:/home/remote/pmcdonou/public_html/src/jboss-as7-linux/
